6177202e6fb3a59d4198715b11bb3d82c41b0bb7
Daniel Sleeth <danielsleeth@gmail.com>
2019-02-12 10:03:36 +0000

    Update eleventy config
    Based off philhawksworth/eleventyone

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,71 @@
-module.exports = function(eleventyConfig) {
-    eleventyConfig.setBrowserSyncConfig({
-      host: "0.0.0.0"
-    });
+const { DateTime } = require("luxon");
 
-    eleventyConfig.addPassthroughCopy("styles");
 
-    return {
-      passthroughFileCopy: true
-    }
+module.exports = function(config) {
+
+  // A useful way to reference to the contect we are runing eleventy in
+  let env = process.env.ELEVENTY_ENV;
+
+  // Layout aliases can make templates more portable
+  config.addLayoutAlias('default', 'layouts/default.liquid');
+
+  // Add some utiliuty filters
+  config.addFilter("squash", require("./src/filters/squash.js") );
+  config.addFilter("dateDisplay", (dateObj, format = "LLL d, y") => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: "utc"
+    }).toFormat(format);
+  });
+
+  // minify the html output
+  // config.addTransform("htmlmin", require("./src/utils/minify-html.js"));
+
+
+  // pass some assets right through
+  config.addPassthroughCopy("./src/site/images");
+
+  // make the seed target act like prod
+  env = (env=="seed") ? "prod" : env;
+  return {
+    dir: {
+      input: "src/site",
+      output: "dist",
+      data: `_data/${env}`
+    },
+    // templateFormats : ["njk", "md"],
+    // htmlTemplateEngine : "njk",
+    // markdownTemplateEngine : "njk",
+    passthroughFileCopy: true
   };
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// module.exports = function(eleventyConfig) {
+//     eleventyConfig.setBrowserSyncConfig({
+//       host: "0.0.0.0"
+//     });
+
+//     eleventyConfig.addPassthroughCopy("styles");
+
+//     return {
+//       passthroughFileCopy: true
+//     }
+//   };
