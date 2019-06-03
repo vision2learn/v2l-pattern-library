e4b11d16c5a4467865a22fa61943de34d78df6a4
Daniel Sleeth <danielsleeth@gmail.com>
2019-06-03 16:23:03 +0100

    Add working first pass

--- src/js/script.js
+++ src/js/script.js
@@ -506,3 +506,66 @@ var Toolkit = (function () {
   });
 
 })();
+
+// LocalStorage (Time to Think)
+(function () {
+
+  // Check localStorage is available
+  // See https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+  function storageAvailable(type) {
+    var storage;
+    try {
+      storage = window[type];
+      var x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    }
+    catch(e) {
+      return e instanceof DOMException && (
+        // everything except Firefox
+        e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === 'QuotaExceededError' ||
+        // Firefox
+        e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+        // acknowledge QuotaExceededError only if there's something already stored
+        (storage && storage.length !== 0);
+    }
+  }
+
+  if(storageAvailable('localStorage')) {
+    console.log('LocalStorage OK');
+
+    // is there a Time to Think component on the page?
+    const timeToThink = document.querySelectorAll('.js-think');
+
+    if(timeToThink.length > 0) {
+      // get this page's dataset from main
+      // creates a unique storage key for the page
+      const mainData = document.querySelector('main').dataset;
+      
+      let keyName = `${mainData.v2lCourse.replace(' ', '_')}_u${mainData.v2lUnit}_s${mainData.v2lSession}_p${mainData.v2lPage}`;
+      
+      let storage = window.localStorage;
+      
+
+      Array.prototype.forEach.call(timeToThink, (think, i) => {
+        keyName = `${keyName}_${i}`;
+        think.addEventListener('blur', function() {
+          if(think.value !== null) {
+            storage.setItem(keyName, think.value);
+          }
+        });
+        
+        // populate the textbox if localstorage values exist
+        if (storage.getItem(keyName) !== null) {
+          think.value = storage.getItem(keyName);
+        }
+      });
+    }
+  }
+})();
