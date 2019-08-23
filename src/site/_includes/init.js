1dc1cf3ec511e0ea1df771fb49b34694f77fd8a0
Daniel Sleeth <danielsleeth@gmail.com>
2019-08-23 12:06:46 +0100

    Add refreshed interactives

---
+++ src/site/_includes/init.js
@@ -0,0 +1,28 @@
+var canvas, stage, exportRoot, anim_container, dom_overlay_container, fnStartAnimation;
+// inc
+function init() {
+	canvas = document.getElementById("canvas");
+	anim_container = document.getElementById("animation_container");
+	dom_overlay_container = document.getElementById("dom_overlay_container");
+	var comp=AdobeAn.getComposition("{{ section.interactive.composition }}");
+	var lib=comp.getLibrary();
+	var loader = new createjs.LoadQueue(false, {{ '"@ViewBag.ApplicationPath/"' | safe if site.env === "dotnet" else '"/images/"' | safe }});
+	loader.addEventListener("fileload", function(evt){handleFileLoad(evt,comp)});
+	loader.addEventListener("complete", function(evt){handleComplete(evt,comp)});
+	var lib=comp.getLibrary();
+  loader.loadManifest(lib.properties.manifest);
+}
+
+function handleFileLoad(evt, comp) {
+	var images=comp.getImages();	
+	if (evt && (evt.item.type == "image")) { images[evt.item.id] = evt.result; }	
+}
+function handleComplete(evt,comp) {
+	//This function is always called, irrespective of the content. You can use the variable "stage" after it is created in token create_stage.
+	var lib=comp.getLibrary();
+	var ss=comp.getSpriteSheet();
+	var queue = evt.target;
+	var ssMetadata = lib.ssMetadata;
+	for(i=0; i<ssMetadata.length; i++) {
+		ss[ssMetadata[i].name] = new createjs.SpriteSheet( {"images": [queue.getResult(ssMetadata[i].name)], "frames": ssMetadata[i].frames} )
+	}
