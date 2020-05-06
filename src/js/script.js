666279704b0cc643a1fd510bc629bea7bb28e639
Daniel Sleeth <danielsleeth@gmail.com>
2020-05-06 16:57:41 +0100

    Add WiP overlay
    Working visually but keyboard trap issues with iframe

--- src/js/script.js
+++ src/js/script.js
@@ -847,3 +847,95 @@ var Toolkit = (function () {
 
   // storage.removeItem(switchKey); // DEBUG!!!!
 })();
+
+// Overlay for activities 
+(function (){
+
+  // Requirements:
+  // Get activity URL
+
+  document.querySelectorAll('[data-v2l-activity').forEach(function(anchor, index){
+    anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      
+      // load the inert polyfill (if needed)
+      const inertPoly = document.createElement("script");
+      inertPoly.src = "/js/inert.js";
+      document.head.appendChild(inertPoly);
+
+      let activeUrl = anchor.href;
+      const body = document.querySelector('body');
+      
+      // get the section's h2 to label the overlay
+      let parentNode = anchor.parentElement.tagName === 'SECTION' ? anchor.parentElement : anchor.parentElement.parentElement;
+      let label = parentNode.querySelector('h2').innerText;
+            
+      body.dataset.v2lOverlay = true;
+
+      const overlay = document.createElement('div');
+      const iframe = document.createElement('iframe');
+      const closeBtn = document.createElement('button');
+
+      closeBtn.addEventListener('click', closeModal);
+      
+      overlay.addEventListener('keydown', function(e) {
+        console.log(e.code);
+        
+        if(e.code == "Escape") {
+          e.preventDefault();
+          closeModal();
+        }
+      });
+
+      iframe.addEventListener('keydown', function(e) {
+        console.log(e.code);
+        
+        if(e.code == "Escape") {
+          e.preventDefault();
+          closeModal();
+        }
+      });
+
+      function closeModal() {
+        body.removeChild(overlay);
+        body.removeAttribute('data-v2l-overlay');
+        Array.prototype.forEach.call(elems, elem => {
+          elem.removeAttribute('inert');
+        });
+        anchor.focus();
+      }
+
+      closeBtn.innerText = "Close activity";
+      overlay.classList.add('c-overlay');
+      iframe.src = activeUrl;
+      iframe.width = "90%";
+      iframe.height = "70%";
+      
+      overlay.append(iframe);
+      overlay.append(closeBtn);
+      overlay.setAttribute('role', 'dialog');
+      overlay.setAttribute('aria-label', label);
+      overlay.setAttribute('tab-index', -1);
+
+      const elems = document.querySelectorAll('body > *');
+      Array.prototype.forEach.call(elems, elem => {
+        elem.setAttribute('inert', 'inert');
+      });
+
+      body.append(overlay);
+      overlay.querySelector('button').focus();
+    });
+  });
+
+  
+  // disable default behaviour
+
+  // Load activity code (iframe?)
+  // Append overlay to DOM
+  // disable page behind
+  // dismiss with button and Esc
+  
+
+  
+
+})();
