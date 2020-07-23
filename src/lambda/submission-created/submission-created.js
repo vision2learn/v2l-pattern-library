25bebefde01376062eced755a71efed2cea8997f
Daniel Sleeth <danielsleeth@gmail.com>
2020-07-23 14:55:42 +0100

    Update form handler

--- src/lambda/submission-created/submission-created.js
+++ src/lambda/submission-created/submission-created.js
@@ -1,15 +1,45 @@
 /* eslint-disable */
 
-const fetch = require('node-fetch');
+/* eslint-disable */
 
-const API_ENDPOINT = "https://icanhazdadjoke.com/";
+// // optionally configure local env vars
+// require('dotenv').config()
 
-exports.handler = async (event, context) => {
-  return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
+// // details in https://css-tricks.com/using-netlify-forms-and-netlify-functions-to-build-an-email-sign-up-widget
+const fetch = require('node-fetch')
+const { GH_TOKEN } = process.env.GitHub_Token
+exports.handler = async event => {
+  const fb = JSON.parse(event.body).payload.feedback
+  console.log(`Recieved feedback: ${fb}`)
+  return fetch('https://api.github.com/repos/mrsleeth/v2l-pattern-library/issues', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Token ${GH_TOKEN}`,
+      'Content-Type': 'application/vnd.github.v3+json',
+    },
+    body: {
+      title: "Test Issue",
+      body: JSON.stringify({ fb })
+    }
+  })
     .then(response => response.json())
-    .then(data => ({
-      statusCode: 200,
-      body: data.joke
-    }))
-    .catch(error => ({ statusCode: 422, body: String(error) }));
-};
+    .then(data => {
+      console.log(`Submitted Issue to GitHub:\n ${data}`)
+    })
+    .catch(error => ({ statusCode: 422, body: String(error) }))
+}
+
+
+// const fetch = require('node-fetch');
+
+// const API_ENDPOINT = "https://icanhazdadjoke.com/";
+
+// exports.handler = async (event, context) => {
+//   return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
+//     .then(response => response.json())
+//     .then(data => ({
+//       statusCode: 200,
+//       body: data.joke
+//     }))
+//     .catch(error => ({ statusCode: 422, body: String(error) }));
+// };
