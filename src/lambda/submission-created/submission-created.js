2129df2289cbb190c7dfbd3983b215c295638167
Daniel Sleeth <danielsleeth@gmail.com>
2020-07-23 15:41:21 +0100

    Use the correct blinking var

--- src/lambda/submission-created/submission-created.js
+++ src/lambda/submission-created/submission-created.js
@@ -7,7 +7,7 @@
 
 // // details in https://css-tricks.com/using-netlify-forms-and-netlify-functions-to-build-an-email-sign-up-widget
 const fetch = require('node-fetch')
-const { GH_TOKEN } = process.env.GitHub_Token
+const { GH_TOKEN } = process.env.GitHub_Auth
 exports.handler = async event => {
   const fb = JSON.parse(event.body).payload.feedback
   console.log(`Recieved feedback: ${fb}`)
