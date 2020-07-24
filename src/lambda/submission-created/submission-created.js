fde750d6f30595dd53a57e1906ac61e00737b3f9
Daniel Sleeth <danielsleeth@gmail.com>
2020-07-24 12:03:13 +0100

    Tidy up submission-created.js

--- src/lambda/submission-created/submission-created.js
+++ src/lambda/submission-created/submission-created.js
@@ -1,33 +1,15 @@
 /* eslint-disable */
 
-/* eslint-disable */
-
-// // optionally configure local env vars
-// require('dotenv').config()
-
-// // details in https://css-tricks.com/using-netlify-forms-and-netlify-functions-to-build-an-email-sign-up-widget
 const fetch = require('node-fetch')
 const { GitHub_Auth } = process.env
+
 exports.handler = async event => {
   const fb = JSON.parse(event.body).payload.data
-  // console.log(`Recieved feedback: ${JSON.stringify(fb)}`)
-
-  const issueBody = `
-    - Reviewer: ${fb.name}\n
-    - Email: ${fb.email}\n
-    - User Agent: ${fb.UA}\n\n
-
-    ### Details
-    ${fb.message}
-  `;
 
   const GHJSON = {
     title: `Issue on page ${fb.page}`,
-    body: issueBody
+    body: `- Reviewer: ${fb.name}\n- Email: ${fb.email}\n- User Agent: ${fb.UA}\n\n### Details\n\n${fb.message}`
   }
-
-  console.log(GHJSON);
-  console.log(JSON.stringify(GHJSON));
 
   return fetch('https://api.github.com/repos/mrsleeth/v2l-pattern-library/issues', {
     method: 'POST',
@@ -35,7 +17,8 @@ exports.handler = async event => {
       'Authorization': `Token ${GitHub_Auth}`,
       'Content-Type': 'application/vnd.github.v3+json',
     },
-    body: JSON.stringify(GHJSON)
+    body: JSON.stringify(GHJSON),
+    labels: ['Feedback from Review']
   })
     .then(response => response.json())
     .then(data => {
@@ -43,18 +26,3 @@ exports.handler = async event => {
     })
     .catch(error => ({ statusCode: 422, body: String(error) }))
 }
-
-
-// const fetch = require('node-fetch');
-
-// const API_ENDPOINT = "https://icanhazdadjoke.com/";
-
-// exports.handler = async (event, context) => {
-//   return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
-//     .then(response => response.json())
-//     .then(data => ({
-//       statusCode: 200,
-//       body: data.joke
-//     }))
-//     .catch(error => ({ statusCode: 422, body: String(error) }));
-// };
