8dd958796ca89d98a8fcff13ea420ddd5adc1a38
Daniel Sleeth <danielsleeth@gmail.com>
2020-08-05 16:16:10 +0100

    Add feedback instructions to login page

--- src/site/content/content.11tydata.js
+++ src/site/content/content.11tydata.js
@@ -1,5 +1,5 @@
 module.exports = function() {
-  let layout = (process.env.ELEVENTY_ENV === 'dotnet') ? 'course-stub' : 'course2';
+  // let layout = (process.env.ELEVENTY_ENV === 'dotnet') ? 'course-stub' : 'course2';
 
   return {
     date: "2019-01-01",
