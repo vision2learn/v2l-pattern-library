0274fb9a0381b23ba3c2e9e1d49ff925f2a11d66
Daniel Sleeth <danielsleeth@gmail.com>
2020-12-21 14:45:58 +0000

    Move, rename BA data

--- .eleventy.js
+++ .eleventy.js
@@ -119,7 +119,7 @@ module.exports = function(config) {
       data: "_data",
       layouts: "_layouts"
     },
-    templateFormats : ["njk", "liquid", "html", "md","11ty.js"],
+    templateFormats : ["njk", "liquid", "html", "md", "11ty.js"],
     dataTemplateEngine: "njk",
     passthroughFileCopy: true
   };
