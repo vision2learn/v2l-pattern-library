2e82f93ab298019281ede48885e875fddf027cc8
Daniel Sleeth <danielsleeth@gmail.com>
2021-01-13 14:50:11 +0000

    Add dotnetID to CourseStructure.xml template

--- src/site/course-structure/course-structure.11tydata.js
+++ src/site/course-structure/course-structure.11tydata.js
@@ -6,9 +6,7 @@ module.exports = {
       let sortedMap = new Map();
 
       for(unit of unitMap.keys()) {
-        console.log(data.pagination.items[0], unit);
         let sessionMap = new Map(Object.entries(data.courses[data.pagination.items[0]][unit]).sort());
-        // console.log(unit, sessionMap);
         sortedMap.set(unit, sessionMap);
       }
 
