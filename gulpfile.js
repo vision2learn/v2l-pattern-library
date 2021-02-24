b6fd5e6510ad7bec15b6b38d03879e77dd3e3501
Daniel Sleeth <danielsleeth@gmail.com>
2021-02-24 14:18:20 +0000

    Handle missing banners gracefully

--- gulpfile.js
+++ gulpfile.js
@@ -6,36 +6,22 @@ const gulp  = require('gulp');
  */
 require('require-dir')('./gulp-tasks');
 
-
-
 /*
   Watch folders for changess
 */
 gulp.task("watch", function() {
   gulp.watch('./src/scss/**/*.scss', gulp.parallel('css'));
   gulp.watch('./src/js/**/*.js', gulp.parallel('js'));
-  gulp.watch('./src/image/**/*.jpg', gulp.parallel('images'));
 });
 
 
 /*
   Let's build this sucker.
 */
-
-
-if(process.env.ELEVENTY_ENV === 'dev' && 1 === 1) {
-  gulp.task('build', gulp.parallel(
-    'css',
-    'js'
-  ));
-} 
-else {
-  gulp.task('build', gulp.parallel(
-    'css',
-    'js'
-  ));
-}
-
+gulp.task('build', gulp.parallel(
+  'css',
+  'js'
+));
 
 /*
   Build and watch things during dev
