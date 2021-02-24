ef8289933ceaf0d2ef9d5b4d6c078385babb8a44
Daniel Sleeth <danielsleeth@gmail.com>
2021-02-24 16:05:52 +0000

    Update .eleventy.js
    ~ switch for dotnet images

--- .eleventy.js
+++ .eleventy.js
@@ -20,7 +20,7 @@ async function imageShortcode(src, cls, alt, sizes) {
   }
 
   const extension = path.extname(src);
-  let tilde = process.env.ELEVENTY_ENV !== 'dev' ? '~' : '';
+  let tilde = process.env.ELEVENTY_ENV === 'dotnet' ? '~' : '';
   let formats = extension === '.png' ? ["webp", "png"] : ["webp", "jpeg"];
   let metadata = await Image(fullSrc.toLowerCase(), {
     widths: [400, 600, 800, 1000, 2000],
