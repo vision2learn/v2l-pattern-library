8c7bbe109c55c106172ea7d92affaeb1095e2ec1
Daniel Sleeth <danielsleeth@gmail.com>
2022-02-08 17:12:22 +0000

    Document .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -10,10 +10,9 @@ const fs = require("fs");
 const MarkdownIt = require('markdown-it');
 const ssri = require('ssri');
 
-let tilde = process.env.ELEVENTY_ENV === 'dotnet' ? '~' : '';
-let missingCaptions = [];
-let missingTranscripts = [];
+let tilde = process.env.ELEVENTY_ENV === 'dotnet' ? '~' : ''; // We need the tilde for when we build .NET pages
 
+// Resizes images and creates PNG, JPG, and WEBP versions dependent on original format
 async function imageShortcode(src, cls, alt, sizes) {
   src = src.startsWith('/') ? src.replace('/', '') : src;
   let fullSrc = `src/site/${src.toLowerCase()}`;
@@ -21,8 +20,8 @@ async function imageShortcode(src, cls, alt, sizes) {
   try {
     fs.accessSync(fullSrc, fs.constants.F_OK);
   } catch (err) {
-      console.error(`${fullSrc} not found. Using placeholder`);
-      fullSrc = "src/site/images/banners/placeholder.jpg";
+    console.error(`${fullSrc} not found. Using placeholder`);
+    fullSrc = "src/site/images/banners/placeholder.jpg";
   }
 
   const extension = path.extname(src);
@@ -51,6 +50,7 @@ async function imageShortcode(src, cls, alt, sizes) {
   return Image.generateHTML(metadata, imageAttributes);
 }
 
+// Function that powers the markdownify filter
 function markdown(copy) {
   let md = new MarkdownIt();
       
@@ -63,21 +63,23 @@ function markdown(copy) {
   }
 }
 
-function logMissing(files) {
-  files.forEach(file => {
-    console.log('No VTT for: ', file);
-  });
-}
-
 module.exports = function(config) {
 
-  // A useful way to reference to the contect we are runing eleventy in
+  // A useful way to reference to the context we are runing eleventy in
   let env = process.env.ELEVENTY_ENV;
 
-  config.addPlugin(pluginSyntaxHighlight);
-  config.addDataExtension("yaml", contents => yaml.safeLoad(contents));
-  config.addPlugin(eleventyNavigationPlugin);
-  config.addNunjucksAsyncShortcode("image", imageShortcode);
+  // Layout aliases can make templates more portable
+  config.addLayoutAlias('default', 'default.njk');
+
+  config.addPlugin(pluginSyntaxHighlight); // see => https://www.11ty.dev/docs/plugins/syntaxhighlight/ (highlights code examples in Documentation and Understanding Coding)
+  config.addPlugin(eleventyNavigationPlugin); // see => https://www.11ty.dev/docs/plugins/navigation/ (Only used on Documentation section)
+  config.addDataExtension("yaml", contents => yaml.safeLoad(contents)); // see => https://www.11ty.dev/docs/data-custom/ (allows you to use YAML in _data folder)
+  config.addNunjucksAsyncShortcode("image", imageShortcode); // see => https://www.11ty.dev/docs/plugins/image/ (responsive images, specifically on the banners)
+  
+  /* 
+    See => https://www.npmjs.com/package/ssri
+    Uses this package to generate sub-resource integrity hashes for JS and CSS
+  */
   config.addAsyncShortcode("ssri", async function(file) {
     let filePath;  
     
@@ -103,16 +105,19 @@ module.exports = function(config) {
     }
   });
 
-  // Layout aliases can make templates more portable
-  config.addLayoutAlias('default', 'default.liquid');
-
+  
+  /*
+    Used by course-rev.njk
+    Takes the pagination.data string and returns an object with the course, unit and session names for the current page
+    These names are the folder names in the `_data/courses` folder
+  */
   config.addFilter('folders', paginationStr => {
-    let objStr = paginationStr;
+    let objStr = paginationStr; // format = courses['course-name']['unit-name']['session-name']
 
     // clean up the object
-    objStr = objStr.replace("courses", "").replace(/]/g, "").replace(/'/g, "");
+    objStr = objStr.replace("courses", "").replace(/]/g, "").replace(/'/g, ""); // objStr => [course-name[unit-name[session-name
 
-    objStr = objStr.split("[");
+    objStr = objStr.split("["); // array => ['course-name', 'unit-name', 'session-name']
 
     objStr.reverse().pop(); // remove the empty string created by split()
     objStr = objStr.reverse();
@@ -124,26 +129,34 @@ module.exports = function(config) {
     }
   });
 
+  /*
+    Uses Node's built-in path module to get file info => https://nodejs.org/api/path.html
+    Builds up and returns the <video> tag 
+    Looks for local video files as fallback but expects videos to be in Azure Blob Storage
+    Addtionally, it looks for captions (.vtt) and transcripts (.md) in the 'src/site/videos' folder and adds these to the page if present
+  */
   config.addShortcode('video', (file, id) => {
-    let ext = path.extname(file);
-    let basename = path.basename(file, ext);
-    let dirname = `${path.dirname(file)}/`;
     let track = '';
     let transcript = '';
-    let multi = basename.indexOf('_pc') > 0 ? true : false;
+    let ext = path.extname(file); // file extension
+    let basename = path.basename(file, ext); // filename without extension
+    let dirname = `${path.dirname(file)}/`;
+    let multi = basename.indexOf('_pc') > 0 ? true : false; // we need to handle instances where there two versions of a video (Mac and PC on IT2)
     let filesToCheck = [basename];
     let vidFilePath = `https://kpcontent.blob.core.windows.net/$web/resources/v2l/720/${file}`;
 
+    // Load the placeholder video if 'placeholder' is set in the YAML file
     if(basename.indexOf('placeholder') !== -1) {
       vidFilePath = 'https://kpcontent.blob.core.windows.net/$web/resources/v2l/720/placeholder.mp4';
       multi = false;
     }
 
+    // Add filename of Mac version of video to the array of files to check
     if(multi) {
       filesToCheck.push(basename.replace('_pc', '_mac'))
     }
 
-    // Is there a VTT for this video?
+    // If there's a VTT for this video, build up the <track> element
     try {
       fs.accessSync(`src/site/videos/captions/vtt/${dirname}${basename}.vtt`, fs.constants.F_OK);
 
@@ -152,14 +165,13 @@ module.exports = function(config) {
       track = `<track default label="English" kind="captions" srclang="en" src="${tilde}/videos/captions/vtt/${dirname + basename}.vtt">`;
     } catch (err) {
       console.log(`No VTT file for ${dirname + file}`);
-      missingCaptions.push(file);
     }
 
     filesToCheck.forEach((file, index) => {
       let type = !multi ? [''] : ['(PC users)', '(Mac users)'];
 
+      // Look for a transcript (.md) file, parse it and convert Markdown to HTML
       try {
-
         fs.accessSync(`src/site/videos/transcripts/${dirname + file}.md`, fs.constants.F_OK);
         let transcriptContent = fs.readFileSync(`src/site/videos/transcripts/${dirname}${file}.md`, 'utf-8', (err, data) => {
           if (err) throw err;
@@ -174,7 +186,6 @@ module.exports = function(config) {
         `;
       } catch (err) {
         console.log(`No transcript for ${dirname + file}`);
-        missingTranscripts.push(file)      
       }
     });
     
@@ -203,22 +214,34 @@ module.exports = function(config) {
       `;
   });
 
+  /* 
+    Parse Markdown in YAML files and return HTML
+    Usage {{ content | markdownify }}
+  */
   config.addFilter("markdownify", value => {
     return markdown(value);
   });
 
+  // REMOVE?
   config.addFilter("yamlify", value => {
     return json2yaml.stringify(value);
   });
 
+  /*
+    Used on the colours documentation page
+    Takes HSL values and returns HEX 
+    See => https://www.npmjs.com/package/@paulobontempo/hsl-to-hex
+  */
   config.addFilter("hexify", value => {
     let hsl = value.replace(/[^0-9^,]+/g, '').split(',');
     return hslToHex(hsl[0], hsl[1], hsl[2]);
   });
 
-  
+  /*
+    Course data objects can be returned in random order so we need to sort them
+    This is why we prepend 01, 02 to the folder names
+  */
   config.addFilter("courseinfo", unit => {
-
     if(unit) {
       return {
         keysOrdered: Object.keys(unit).sort(),
@@ -232,10 +255,14 @@ module.exports = function(config) {
   });
 
   // Pass in an object, return the keys?
+  // REMOVE?
   config.addFilter("keys", obj => {
     return Object.keys(obj);
   });
 
+  /*
+    Sentence case filter but handles 'IT' so that it doesn't get transformed to 'It'
+  */
   config.addFilter("formattitle", value => {
     if(!value) {
       console.log("...Error: ", value);
@@ -275,6 +302,7 @@ module.exports = function(config) {
     return formatted;
   });
 
+  // REMOVE?
   config.addFilter("trimit", function(value, start, end) {
     start = start ? start : 0;
 
@@ -286,9 +314,11 @@ module.exports = function(config) {
     }
   });
 
+  // Creates a unique value at build time for page elements so there are no conflicts
   config.addFilter("uniqueID", val => {return `${val}_${Math.floor(Math.random(+new Date)*9999999999999)}` });
 
   // Add some utiliuty filters
+  // REMOVE?
   config.addFilter("squash", require("./src/filters/squash.js") );
   config.addFilter("dateDisplay", (dateObj, format = "LLL d, y") => {
     return DateTime.fromJSDate(dateObj, {
